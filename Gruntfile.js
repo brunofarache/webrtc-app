2a9fad168dba620494f579a00b0d9eaa1211a621
Bruno Farache <bruno.farache@liferay.com>
2013-11-30 23:20:27 -0300

    watch task

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,12 +4,18 @@ module.exports = function(grunt) {
 
 	grunt.initConfig({
 		jshint: {
-			files: ['server.js', '**/client.js'],
+			files: ['server.js', 'public/**/client.js'],
 			options: grunt.file.readJSON('.jshintrc')
+		},
+
+		watch: {
+			files: ['server.js', '**/client.js'],
+			tasks: ['default']
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	grunt.registerTask('default', ['jshint']);
 };
