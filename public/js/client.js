9e7a1f5de0534485a00ef1dcda77bb930512f046
Bruno Farache <bruno.farache@liferay.com>
2013-11-30 21:57:45 -0300

    js beautify

--- public/js/client.js
+++ public/js/client.js
@@ -1,240 +1,241 @@
-(function (window) {
+(function(window) {
 
-var document = window.document,
-	navigator = window.navigator;
+	var document = window.document,
+		navigator = window.navigator;
 
-if (!navigator.getUserMedia) {
-	navigator.getUserMedia =
-		navigator.getUserMedia ||
-		navigator.webkitGetUserMedia ||
-		navigator.mozGetUserMedia ||
-		navigator.msGetUserMedia;
-}
+	if (!navigator.getUserMedia) {
+		navigator.getUserMedia =
+			navigator.getUserMedia ||
+			navigator.webkitGetUserMedia ||
+			navigator.mozGetUserMedia ||
+			navigator.msGetUserMedia;
+	}
 
-if (!window.URL) {
-	window.URL =
-		window.URL ||
-		window.webkitURL ||
-		window.msURL ||
-		window.oURL;
-}
+	if (!window.URL) {
+		window.URL =
+			window.URL ||
+			window.webkitURL ||
+			window.msURL ||
+			window.oURL;
+	}
 
-if (!window.RTCPeerConnection) {
-	window.RTCPeerConnection =
-		window.mozRTCPeerConnection ||
-		window.webkitRTCPeerConnection;
-}
+	if (!window.RTCPeerConnection) {
+		window.RTCPeerConnection =
+			window.mozRTCPeerConnection ||
+			window.webkitRTCPeerConnection;
+	}
 
-if (!window.RTCIceCandidate) {
-	window.RTCIceCandidate = window.mozRTCIceCandidate;
-}
+	if (!window.RTCIceCandidate) {
+		window.RTCIceCandidate = window.mozRTCIceCandidate;
+	}
 
-var app = {
-	channels: {},
-	peers: {},
-	room: null,
-	stream: null,
+	var app = {
+		channels: {},
+		peers: {},
+		room: null,
+		stream: null,
 
-	start: function() {
-		var instance = this,
-			video = document.getElementById('local');
+		start: function() {
+			var instance = this,
+				video = document.getElementById('local');
 
-		instance._setMain(video);
+			instance._setMain(video);
 
-		navigator.getUserMedia(
-			{
-				'audio': true,
-				'video': true
-			},
-			function (stream) {
-				instance._attachStream(video, stream);
-				video.muted = true;
-				instance.stream = stream;
+			navigator.getUserMedia({
+					'audio': true,
+					'video': true
+				},
+				function(stream) {
+					instance._attachStream(video, stream);
+					video.muted = true;
+					instance.stream = stream;
 
-				socket.emit('join', location.hash);
-			},
-			function () {
-				console.log('cannot access camera');
-			}
-		);
-	},
+					socket.emit('join', location.hash);
+				},
+				function() {
+					console.log('cannot access camera');
+				}
+			);
+		},
 
-	_attachStream: function(video, stream) {
-		video.autoplay = true;
-		video.src = window.URL.createObjectURL(stream);
-	},
+		_attachStream: function(video, stream) {
+			video.autoplay = true;
+			video.src = window.URL.createObjectURL(stream);
+		},
 
-	_addIceCandidate: function(message) {
-		var instance = this,
-			peer = instance.peers[message.from];
+		_addIceCandidate: function(message) {
+			var instance = this,
+				peer = instance.peers[message.from];
 
-		peer.addIceCandidate(new RTCIceCandidate(message.payload));
-	},
+			peer.addIceCandidate(new RTCIceCandidate(message.payload));
+		},
 
-	_answer: function(message) {
-		var instance = this,
-			peer = instance._createPeerConnection(message.from);
+		_answer: function(message) {
+			var instance = this,
+				peer = instance._createPeerConnection(message.from);
 
-		instance._setRemoteDescription(message);
+			instance._setRemoteDescription(message);
 
-		var constraints = {
-			'mandatory': {
-				'OfferToReceiveAudio': true, 
-				'OfferToReceiveVideo': true
-			}
-		};
-
-		peer.createAnswer(instance._setLocalDescription.bind(instance, peer), null, constraints);
-	},
-
-	_offer: function(id) {
-		var instance = this,
-			peer = instance._createPeerConnection(id);
-
-		peer.createOffer(instance._setLocalDescription.bind(instance, peer));
-	},
-
-	_createPeerConnection: function(id) {
-		var instance = this,
-			servers = {
-				'iceServers': [
-					{'url': 'stun:stun.l.google.com:19302'}
-				]
-			},
-			options = {
-				'optional': [
-					{'RtpDataChannels': true}
-				]
-			},
-			peer = new RTCPeerConnection(servers, options),
-			peers = instance.peers,
-			stream = instance.stream,
-			channels = instance.channels;
-
-		peer.onicecandidate = instance._onIceCandidate.bind(instance, id);
-		peer.onaddstream = instance._onAddStream.bind(instance, id);
-		peer.addStream(stream);
-
-		peer.id = id;
-		peers[id] = peer;
-
-		channels[id] = peer.createDataChannel('chat', {'reliable': false});
-		channels[id].onmessage = instance._onDataChannelMessage.bind(instance);
-
-		return peer;
-	},
-
-	_setLocalDescription: function(peer, description) {
-		var instance = this;
-
-		peer.setLocalDescription(description);
-
-		socket.emit('relay', {
-			'room': instance.room,
-			'to': peer.id,
-			'payload': description
-		});
-	},
-
-	_setMain: function(video) {
-		var instance = this,
-			previous = document.querySelector('video.main'),
-			thumbnails = document.querySelectorAll('video.thumbnail').length;
-
-		video.className = 'main';
-		video.width = window.innerWidth;
-
-		if (previous) {
-			previous.style.top = (thumbnails * 150) + 'px';
-			previous.className = 'thumbnail';
-		}
-	},
-
-	_setRemoteDescription: function(message) {
-		var instance = this,
-			peer = instance.peers[message.from];
-
-		peer.setRemoteDescription(new RTCSessionDescription(message.payload));
-	},
-
-	_onIceCandidate: function(id, event) {
-		var instance = this;
-
-		if (event.candidate) {
-			var payload = {
-				type: 'candidate',
-				sdpMLineIndex: event.candidate.sdpMLineIndex,
-				candidate: event.candidate.candidate
+			var constraints = {
+				'mandatory': {
+					'OfferToReceiveAudio': true,
+					'OfferToReceiveVideo': true
+				}
 			};
+
+			peer.createAnswer(instance._setLocalDescription.bind(instance, peer), null, constraints);
+		},
+
+		_offer: function(id) {
+			var instance = this,
+				peer = instance._createPeerConnection(id);
+
+			peer.createOffer(instance._setLocalDescription.bind(instance, peer));
+		},
+
+		_createPeerConnection: function(id) {
+			var instance = this,
+				servers = {
+					'iceServers': [{
+						'url': 'stun:stun.l.google.com:19302'
+					}]
+				},
+				options = {
+					'optional': [{
+						'RtpDataChannels': true
+					}]
+				},
+				peer = new RTCPeerConnection(servers, options),
+				peers = instance.peers,
+				stream = instance.stream,
+				channels = instance.channels;
+
+			peer.onicecandidate = instance._onIceCandidate.bind(instance, id);
+			peer.onaddstream = instance._onAddStream.bind(instance, id);
+			peer.addStream(stream);
+
+			peer.id = id;
+			peers[id] = peer;
+
+			channels[id] = peer.createDataChannel('chat', {
+				'reliable': false
+			});
+			channels[id].onmessage = instance._onDataChannelMessage.bind(instance);
+
+			return peer;
+		},
+
+		_setLocalDescription: function(peer, description) {
+			var instance = this;
+
+			peer.setLocalDescription(description);
 
 			socket.emit('relay', {
 				'room': instance.room,
-				'to': id,
-				'payload': payload
+				'to': peer.id,
+				'payload': description
 			});
-		}	
-	},
+		},
 
-	_onAddStream: function(id, event) {
-		var instance = this,
-			video = document.createElement('video');
+		_setMain: function(video) {
+			var instance = this,
+				previous = document.querySelector('video.main'),
+				thumbnails = document.querySelectorAll('video.thumbnail').length;
 
-		video.id = id;
-		document.body.appendChild(video);
-		instance._setMain(video);
+			video.className = 'main';
+			video.width = window.innerWidth;
 
-		instance._attachStream(video, event.stream);
-	},
+			if (previous) {
+				previous.style.top = (thumbnails * 150) + 'px';
+				previous.className = 'thumbnail';
+			}
+		},
 
-	_onDataChannelMessage: function(event) {
-		console.log('_onDataChannelMessage', event);
-	},
+		_setRemoteDescription: function(message) {
+			var instance = this,
+				peer = instance.peers[message.from];
 
-	_onLeave: function(id) {
-		var instance = this,
-			video = document.getElementById(id),
-			peers = instance.peers,
-			peer = peers[id];
+			peer.setRemoteDescription(new RTCSessionDescription(message.payload));
+		},
 
-		peer.close();
-		delete peers[id];
+		_onIceCandidate: function(id, event) {
+			var instance = this;
 
-		video.parentNode.removeChild(video);
-	}
-};
+			if (event.candidate) {
+				var payload = {
+					type: 'candidate',
+					sdpMLineIndex: event.candidate.sdpMLineIndex,
+					candidate: event.candidate.candidate
+				};
 
-var socket = io.connect();
+				socket.emit('relay', {
+					'room': instance.room,
+					'to': id,
+					'payload': payload
+				});
+			}
+		},
 
-socket.on('relay', function (message) {
-	var type = message.payload.type;
+		_onAddStream: function(id, event) {
+			var instance = this,
+				video = document.createElement('video');
 
-	if (type === 'offer') {
-		app._answer(message);
-	}
-	else if (type === 'answer') {
-		app._setRemoteDescription(message);
-	}
-	else if (type == 'candidate') {
-		app._addIceCandidate(message);
-	}
-});
+			video.id = id;
+			document.body.appendChild(video);
+			instance._setMain(video);
 
-socket.on('created', function (room) {
-	app.room = room;
-});
+			instance._attachStream(video, event.stream);
+		},
 
-socket.on('join', function (id) {
-	app._offer(id);
-});
+		_onDataChannelMessage: function(event) {
+			console.log('_onDataChannelMessage', event);
+		},
 
-socket.on('leave', function (id) {
-	app._onLeave(id);
-});
+		_onLeave: function(id) {
+			var instance = this,
+				video = document.getElementById(id),
+				peers = instance.peers,
+				peer = peers[id];
 
-window.onbeforeunload = function(event) {
-	socket.emit('leave', app.room);
-};
+			peer.close();
+			delete peers[id];
 
-window.app = app;
+			video.parentNode.removeChild(video);
+		}
+	};
+
+	var socket = io.connect();
+
+	socket.on('relay', function(message) {
+		var type = message.payload.type;
+
+		if (type === 'offer') {
+			app._answer(message);
+		}
+		else if (type === 'answer') {
+			app._setRemoteDescription(message);
+		}
+		else if (type == 'candidate') {
+			app._addIceCandidate(message);
+		}
+	});
+
+	socket.on('created', function(room) {
+		app.room = room;
+	});
+
+	socket.on('join', function(id) {
+		app._offer(id);
+	});
+
+	socket.on('leave', function(id) {
+		app._onLeave(id);
+	});
+
+	window.onbeforeunload = function(event) {
+		socket.emit('leave', app.room);
+	};
+
+	window.app = app;
 
 })(window);
--- server.js
+++ server.js
@@ -1,24 +1,26 @@
 var app = require('http').createServer(handler),
-	io = require('socket.io').listen(app, { log: false }),
+	io = require('socket.io').listen(app, {
+		log: false
+	}),
 	static = require('node-static'),
 	server = new static.Server('./public');
 
 app.listen(80);
 
 function handler(request, response) {
-	request.addListener('end', function () {
+	request.addListener('end', function() {
 		server.serve(request, response);
 	}).resume();
 }
 
-io.sockets.on('connection', function (socket) {
-	socket.on('relay', function (message) {
+io.sockets.on('connection', function(socket) {
+	socket.on('relay', function(message) {
 		message.from = socket.id;
 
 		io.sockets.socket(message.to).emit('relay', message);
 	});
 
-	socket.on('join', function (room) {
+	socket.on('join', function(room) {
 		if (room === '') {
 			room = 'default';
 		}
@@ -37,7 +39,7 @@ io.sockets.on('connection', function (socket) {
 		socket.join(room);
 	});
 
-	socket.on('leave', function (room) {
+	socket.on('leave', function(room) {
 		io.sockets.in(room).emit('leave', socket.id);
 
 		socket.leave(room);
